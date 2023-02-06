6a06a7c5529014d459f0967fa63d803f6d32a41c
DESKTOP-TV3U1VB\Capa <capaseitadavid@gmail.com>
2023-02-06 17:01:23 +0000

    undo gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBQYj7C-qNqTiF_30Pwhl-SqDXyfFgAudA",
+    authDomain: "fivemeals.firebaseapp.com",
+    projectId: "fivemeals",
+    storageBucket: "fivemeals.appspot.com",
+    messagingSenderId: "554072201893",
+    appId: "1:554072201893:web:8d5c4dc545fecf010a0b4c",
+    measurementId: "G-45TK7G9PSM",
+    vapidKey: "BNnlwD7FgxN9L5i8fChsNpfngxiKaAt3bFHm0TInlAad7j3VxKb-zP-p7h53PrNreJXgK2AQcf01fSFCdMe4RZA"
+  },
+};
